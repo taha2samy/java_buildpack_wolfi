4bee15b60473bc393d2bd90b7605858dd9f0c48e
taha2samy <samyt8435@gmail.com>
2026-03-25 07:02:44 +0200

    feat: add Bouncy Castle FIPS and Java security properties for native secure random.

--- fips-java-shim/scripts/jre.py
+++ fips-java-shim/scripts/jre.py
@@ -192,8 +192,13 @@ def setup_env(jre_layer, bc_dest, ks_dir, sec_file):
 
     boot = ":".join(bc_jars)
 
+
     fips_opts = (
         f"-Dorg.bouncycastle.fips.approved_only=true "
+        f"-Dorg.bouncycastle.fips.native_secure_random=true "
+        f"-Dorg.bouncycastle.crypto.fips.seeder=NATIVE "
+        f"-Djava.security.egd=file:/dev/urandom "
+        f"-Djava.security.properties={sec_file.resolve()} "
         f"-Dkeystore.type=BCFKS "
         f"-Djavax.net.ssl.trustStore={ks_dir.resolve()}/cacerts "
         f"-Djavax.net.ssl.trustStoreType=BCFKS "
